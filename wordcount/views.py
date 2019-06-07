3757348200f9ca00e251b40e29eda810106a5ca0
sejun <tonypark0723@likelion.org>
2019-06-08 00:10:49 +0900

    my first django worcount proj

--- wordcount/views.py
+++ wordcount/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request,'index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request,'about.html')
 
 def result(request):
     text = request.GET['fulltext']
